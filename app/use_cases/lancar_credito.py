e653f105c36402a7721da789f254095452417391
vsuguimoto <vinisuguimoto@gmail.com>
2023-05-29 12:10:10 -0400

    feat: create LancarCredito use case

--- app/use_cases/lancar_credito.py
+++ app/use_cases/lancar_credito.py
@@ -1,11 +1,11 @@
 from app.frameworks.banco_de_dados import BancoDeDados
 from app.entities.lancamento import Lancamento
 
-# class LancarCreditoUseCase:
+class LancarCreditoUseCase:
 
-#     def __init__(self, banco_de_dados: BancoDeDados):
-#         self.banco_de_dados = banco_de_dados
+    def __init__(self, banco_de_dados: BancoDeDados):
+        self.banco_de_dados = banco_de_dados
 
-#     def executar(self, categoria_id:int):
-#         self.banco_de_dados.inserir_lancamento(cadegoria_id=categoria_id)
-#         self.banco_de_dados.fechar_conexao()
+    def executar(self, nome, valor, categoria_id:int):
+        self.banco_de_dados.inserir_lancamento(nome=nome, tipo='C', valor=valor, categoria_id=categoria_id)
+        self.banco_de_dados.fechar_conexao()
