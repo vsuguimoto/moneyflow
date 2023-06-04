0972477c84801a2caf034740068e2d145cf1b3c2
vsuguimoto <vinisuguimoto@gmail.com>
2023-06-03 21:07:15 -0400

    feat: user session state

--- main.py
+++ main.py
@@ -3,23 +3,34 @@ import streamlit as st
 from st_pages import Page, Section, show_pages_from_config, add_page_title
 
 
+if 'usuario' not in st.session_state:
+    st.session_state['usuario'] = None
+
+
 show_pages_from_config()
 
 st.header('# Money Flow')
 
-st.write('Antes de iniciarmos, selecione o usuário:')
+if st.session_state['usuario'] == None:
+    
+    st.write('Antes de iniciarmos, selecione o usuário:')
 
-pessoa_control = PessoaController()
-LISTA_PESSOAS = pessoa_control.obter_pessoas()
+    pessoa_control = PessoaController()
+    LISTA_PESSOAS = pessoa_control.obter_pessoas()
 
-user = st.selectbox('Selecione o usuário', LISTA_PESSOAS)
+    usuario = st.selectbox('Selecione o usuário', LISTA_PESSOAS)
 
-cols = st.columns(9)
-with cols[-1]:  
-    usuario_selecionado = st.button('Login')
+    cols = st.columns(9)
+    with cols[-1]:  
+        usuario_selecionado = st.button('Login')
 
-if usuario_selecionado:
-    st.success('Sucesso!!')
+    if usuario_selecionado:
+        st.session_state['usuario'] = usuario
+        st.success('Sucesso!!')
 
-st.markdown('---')
+    st.markdown('---')
 
+
+else:
+    # TODO -> Ir para página de Dashboards
+    pass
